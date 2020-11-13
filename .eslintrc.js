a53939ce7908411d229779897d08be674db2e04b
Jim Cresswell <jim.cresswell@gmail.com>
2020-11-13 10:21:32 +0000

    Adding support libraries

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    "jest/globals": true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:json/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:prettier/recommended",
+    "prettier",
+    "prettier/react",
+    "plugin:markdown/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react", "jest", "prettier"],
+  overrides: [
+    {
+      files: ["*.yaml", "*.yml"],
+      plugins: ["yaml"],
+      extends: ["plugin:yaml/recommended"],
+    },
+  ],
+  ignorePatterns: ["yarn.lock"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": ["error"],
+  },
+  globals: {
+    React: "writable",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # obaith
+
 Obaith website
+
+## Notes
+
+# Set up
+
+- `yarn add --dev @testing-library/jest-dom @testing-library/react babel-jest eslint eslint-config-prettier eslint-plugin-jest eslint-plugin-json eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks identity-obj-proxy jest prettier stylelint stylelint-config-sass-guidelines stylelint-config-standard eslint-plugin-markdown@next eslint-plugin-yaml`
